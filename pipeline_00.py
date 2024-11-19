083395dd325b005d3190ed1f15e49c37a6a754d8
jacksonccs <jackson.santiago@grupoboticario.com.br>
2024-11-19 14:15:40 -0300

    program pipeline_00.py

---
+++ pipeline_00.py
@@ -0,0 +1,54 @@
+import os
+import gdown
+import duckdb
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+from duckdb import DuckDBPyRelation
+from pandas import DataFrame
+
+# download all files
+def baixar_os_arquivos_do_google_drive(url_pasta, diretorio_local):
+    # create pasta
+    os.makedirs(diretorio_local, exist_ok=True)
+    # download all files
+    gdown.download_folder(url_pasta, output=diretorio_local, quiet=False, use_cookies=False)
+
+# list all files in pasta
+def listar_aquivos_csv(diretorio):
+    arquivos_csv = []
+    todos_os_arquivos = os.listdir(diretorio)
+    for arquivo in todos_os_arquivos:
+        if arquivo.endswith(".csv"):
+            caminho_completo = os.path.join(diretorio, arquivo)
+            arquivos_csv.append(caminho_completo)
+    return arquivos_csv
+
+# Ler um arquivo csv e retornar um dataframe duckdb
+def ler_csv(caminho_do_arquivo):
+    dataframe_duckdb = duckdb.read_csv(caminho_do_arquivo)
+    ##print(dataframe_duckdb)
+    ##print(type(dataframe_duckdb))
+    return dataframe_duckdb
+
+# Transformation 
+def transformar(df: DuckDBPyRelation) -> DataFrame:
+    df_transformado = duckdb.sql("select *, (quantidade * valor) as total_vendas from df").df()
+    ##print(df_transformado)
+    return df_transformado
+
+# Salva para converter o duckdb em pandas e salvar o dataframe no banco
+def salvar_no_postgres(df_duckdb, tabela):
+    DATABASE_URL = os.getenv("DATABASE_URL") # 
+    engine = create_engine(DATABASE_URL)
+    df_duckdb.to_sql(tabela, con=engine, if_exists='append', index=False)
+
+if __name__ == "__main__":
+    url_pasta = 'https://drive.google.com/drive/folders/19flL9P8UV9aSu4iQtM6Ymv-77VtFcECP'
+    diretorio_local = './pasta_gdown'
+    
+    ##baixar_os_arquivos_do_google_drive(url_pasta, diretorio_local)
+    arquivos = listar_aquivos_csv(diretorio_local)
+    data_frame_duckdb = ler_csv(arquivos)
+    transformar(data_frame_duckdb)
---
+++ scripts.sql
@@ -0,0 +1,35 @@
+--drop table vendas
+create table vendas(
+	venda_id serial primary key,
+	data_venda date not null,
+	valor decimal(10,2) not null,
+	quantidade int not null,
+	cliente_id int not null,
+	categoria varchar(255) not null
+)
+
+--drop table vendas_calculado
+create table vendas_calculado(
+	venda_id serial primary key,
+	data_venda date not null,
+	valor decimal(10,2) not null,
+	quantidade int not null,
+	cliente_id int not null,
+	categoria varchar(255) not null,
+	total_vendas decimal(10,2) not null
+)
+
+select * from vendas
+select * from vendas_calculado
+
+insert into vendas(data_venda,valor,quantidade,cliente_id,categoria) values 
+('2024-01-05',450.25,2,14,'Brinquedos'),
+('2024-01-05',500.0,4,22,'Livros'),
+('2024-01-05',95.5,5,19,'Alimentos'),
+('2024-01-05',380.75,3,3,'Eletrônicos'),
+('2024-01-05',460.0,7,11,'Livros'),
+('2024-01-05',140.25,9,17,'Roupas'),
+('2024-01-05',360.6,6,1,'Alimentos'),
+('2024-01-05',290.4,1,28,'Brinquedos'),
+('2024-01-05',240.5,8,20,'Eletrônicos'),
+('2024-01-05',155.0,3,45,'Roupas')
